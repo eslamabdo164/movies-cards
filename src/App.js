b2142895cf07218406dc6744eb82b0e52838aa56
eslamabdo <eslam.abdo@live.com>
2019-01-12 00:05:46 +0200

    adding font awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFilm } from '@fortawesome/free-solid-svg-icons'
+library.add(faFilm)
+
 class App extends Component {
   render() {
     return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Navbar,NavbarBrand} from 'reactstrap';
+import {Navbar} from 'reactstrap';
+import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Header = () => {
     return(
-        <div className=''>
+        <div className='cus_nav'>
            <Navbar color="dark text-center" light expand="md">
-                <NavbarBrand className='text-white mx-auto' href="/">Movies Cards</NavbarBrand>
+                <p className='text-white display-4 mx-auto' href="/">
+                <FontAwesomeIcon className='mr-3' icon="film" />
+                Movies Cards</p>
              </Navbar>
         </div>
     )
